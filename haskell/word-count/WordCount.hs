b00bcbb528a0ea2961c0c3dee7a091502619be95
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2014-03-08 14:06:56 +0530

    Solved word count problem

--- haskell/word-count/WordCount.hs
+++ haskell/word-count/WordCount.hs
@@ -1,7 +1,15 @@
-module WordCount ( wordCount ) where 
-import Data.Char
-import Data.List
+module WordCount ( wordCount ) where
+
+import Data.Char ( isAlphaNum, isSpace, toLower )
+import Data.Map  ( Map, fromListWith )
+
+wordCount :: String -> Map String Int
+wordCount  = fromListWith ( + ) . flip  zip ( repeat 1 ) . token
+
+token :: String -> [ String ]
+token = words . map ( toLower . replace ) 
+
+replace :: Char -> Char
+replace c = if isAlphaNum c || isSpace c then c else ' '
 
 
-wordCount :: String -> String 
-wordCound s =   
