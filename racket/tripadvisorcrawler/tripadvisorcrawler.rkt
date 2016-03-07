829c9db3ac962e2bf8b748c49ea42170963dad66
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2016-03-08 04:11:21 +0530

    Added code for extracting information from html page

--- racket/tripadvisorcrawler/tripadvisorcrawler.rkt
+++ racket/tripadvisorcrawler/tripadvisorcrawler.rkt
@@ -52,11 +52,31 @@
 ;write a function that fetch the name of hotel, review count, rank, and url
 ;function takes a url of tripadvisor page and return above mentioned data.
 ;This one is bit complecated because it executes java script so think for a
-;method which executes java script racket.
+;method which executes java script in  racket. For the momenet, save the page in
+;browser and do the parsing. When I have solution then replace it with url.
 (define (hotel-info url)
   (html->xexp
    (get-pure-port
     (string->url url))))
 
+;helper function to extract name, review url, number of reviews, and
+; rating. Add data algebraic data structure
 
-(hotel-info "https://www.tripadvisor.in/Hotels-g187147-oa1770-Paris_Ile_de_France-Hotels.html")
+(struct hotel (name url num-reviews rating))
+
+(define (page-struct-info div-node)
+  (let* ([page-title ((sxpath "(//div[@class=\"listing_title\"]/a)/text()") div-node)]
+         [page-url (sxml:attr (first ((sxpath "(//div[@class=\"listing_title\"]/a)") div-node)) 'href)]
+         [num-review ((sxpath "(//span[@class=\"more\"]/a)/text()") div-node)]
+         [tripadvisor-rank ((sxpath "(//div[@class=\"slim_ranking\"])/text()") div-node)])
+    (list page-title page-url num-review tripadvisor-rank)))
+         
+;(hotel-info "https://www.tripadvisor.in/Hotels-g60713-oa30-San_Francisco_California-Hotels.html")
+;go with html parsing and plug it later the url
+(define (hotel-info-page page)
+  (let* ([xexp-info-page
+          (html->xexp (open-input-file page))]
+         [doc ((sxpath "(//div[@class=\"listing_info popIndexValidation\"])") xexp-info-page)]
+         [info-list (map page-struct-info doc)])
+    info-list))
+(hotel-info-page "SanFranciscoTripAdvisor.html")
